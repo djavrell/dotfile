efebb6d8e2349e142ea2e27980c1d68aed55bd07
Prouteau Kévin <kevin.prouteau@fabernovel.com>
2017-12-27 22:01:50 +0100

    beginning of the installationscript

---
+++ install.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+# installation script for this dotfile
+
+# check if require program are here and install them if needed
+# install vim
+# install tmux
+# create needed symlink
+  # zshrc
+  # git
+  # vim
+  # tmux
