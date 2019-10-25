eb0bceebaa8b359db4d4df34fcca2af8e970f021
Prouteau Kévin <kevin.prouteau@epitech.eu>
2019-10-25 19:06:14 +0200

    though

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,19 @@
 #! /bin/sh
 
+# pouvoir trouver la distro linux en plus au cas où
 SYSTEM=`uname -s | tr '[:upper:]' '[:lower:]'`
+
+# charger les variable en fonction d'un fichier nommé trouvable grace à uname
+#
+# files:
+#   - variables_<os>.sh
+#   - packages_<os>.sh
+#   - symlinks_<os>.sh
+#
+# variables -> ce fichier vont definir les variables suivantes:
+#   - INSTALLER_TOOLS (aka: pacman, brew, ...)
+#   -
+
 
 case $SYSTEM in
   "darwin")
