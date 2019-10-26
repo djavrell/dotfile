fdc7862adf776138f5abdbec7afbe3545e0b72a1
Prouteau Kévin <kevin.prouteau@epitech.eu>
2019-10-26 18:23:02 +0200

    fix syntax error

--- scripts/linux/variables.sh
+++ scripts/linux/variables.sh
@@ -4,10 +4,11 @@ INSTALLER="sudo pacman -S"
 INSTAL_ALL="sudo pacman -Sa --needed -"
 
 function pre_install() {
+  echo "pre_install hook (do nothing here)"
 }
 
 # $1: file containing all package to install
-function install_all() {
+function install_packages() {
   echo "Installing ...."
   $INSTAL_ALL < $1
 }
