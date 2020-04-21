22198336071b347074405758cbe0d508b148e623
Prouteau Kévin <kevin.prouteau1@gmail.com>
2020-04-21 17:06:58 +0200

    rename file

--- shell/os/darwin/load.sh
+++
@@ -1,9 +0,0 @@
-COLORS="$SYSTEM_FOLDER/gruvbox_256palette_osx.sh"
-
-test -f $COLORS && . $COLORS
-
-# Python
-path=( "/Users/kpr/Library/Python/3.7/bin" $path )
-
-# JAVA
-export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.8.0_144.jdk/Contents/Home"
