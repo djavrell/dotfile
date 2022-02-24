e5aea5d47f419d97cb736a0fcec0503a531a7de5
Prouteau Kévin <kevin.prouteau1@gmail.com>
2022-02-24 15:50:03 +0100

    [nvim]: fix on hop.nvim conf

--- nvim/after/plugin/Hop.lua
+++ nvim/after/plugin/Hop.lua
@@ -3,9 +3,11 @@ require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
 local mapOpt = { silent = true, noremap = true }
 
 vim.api.nvim_set_keymap(''  , 'f', ":HopWordAC<CR>",            mapOpt)
-vim.api.nvim_set_keymap('o' , 't', ":HopWordCurrentLineAC<CR>", mapOpt)
+vim.api.nvim_set_keymap('o' , 'f', ":HopChar1CurrentLineAC<CR>", mapOpt)
+vim.api.nvim_set_keymap('o' , 't', ":HopChar1CurrentLineAC<CR>", mapOpt)
+
 vim.api.nvim_set_keymap(''  , 'F', ":HopWordBC<CR>",            mapOpt)
-vim.api.nvim_set_keymap('o' , 'T', ":HopWordCurrentLineBC<CR>", mapOpt)
+vim.api.nvim_set_keymap('o' , 'T', ":HopChar1CurrentLineBC<CR>", mapOpt)
 
 vim.api.nvim_set_keymap('n', '<leader>k', ":HopLineBC<CR>", mapOpt)
 vim.api.nvim_set_keymap('n', '<leader>j', ":HopLineAC<CR>", mapOpt)
