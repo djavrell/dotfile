01bac7c78495d684c007a6448276c68c44c4bfc1
Prouteau Kévin <kevin.prouteau1@gmail.com>
2022-05-04 10:46:19 +0200

    [nvim] fix after cmp update

--- nvim/after/plugin/completion.lua
+++ nvim/after/plugin/completion.lua
@@ -133,17 +133,23 @@ cmp.setup({
 
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline('/', {
-  sources = {
-    { name = 'buffer' }
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {{ name = 'buffer' }},
+  view = {
+    entries = { name="custom" }
   }
 })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
   sources = cmp.config.sources({
     { name = 'path' },
     { name = 'cmdline' }
-  })
+  }),
+  view = {
+    entries = { name="custom" }
+  }
 })
 
 local servers = {
