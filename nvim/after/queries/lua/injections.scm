8a24ebb8a1dd332037084538e26c80e69396a9a6
Prouteau Kévin <kevin.prouteau1@gmail.com>
2022-06-07 19:42:17 +0200

    [nvim add viml injection in lua autocmd augroup api

---
+++ nvim/after/queries/lua/injections.scm
@@ -0,0 +1,12 @@
+((function_call
+  name: (_) @autocmd
+  arguments: (arguments
+               (_)
+               (table_constructor (field
+                                    name: (_) @key
+                                    value: (string content: _ @vim))
+               ))
+  )
+ (#any-of? @autocmd "autocmd" "api.nvim_create_autocmd" "vim.api.nvim_create_autocmd")
+ (#eq? @key "command")
+)
--- nvim/lua/djavrell/auto_cmd.lua
+++ nvim/lua/djavrell/auto_cmd.lua
@@ -16,47 +16,20 @@ vim.api.nvim_create_autocmd("BufEnter", {
     if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match('NvimTree_') ~= nil then
       vim.cmd "quit"
     end
-  end
+  end,
 })
 
-api.nvim_create_autocmd("TextYankPost", {
+autocmd("TextYankPost", {
   group = M.yank,
   callback = function()
     vim.highlight.on_yank { higroup='IncSearch', timeout=200 }
   end
 })
 
-
---[[
-test for TS query
-
-((function_call
-  name: (_) @_vimcmd_identifier
-  arguments: (arguments (string content: _ @vim)))
-  (#any-of? @_vimcmd_identifier "vim.cmd" "vim.api.nvim_command" "vim.api.nvim_exec"))
---]]
-vim.cmd [[
-  augroup Foo
-  augroup END
-]]
-
---[[
-((function_call
-  name: (_) @autocmd
-  arguments: [
-    (table_constructor) @table <-- Error here
-  ])
- (#any-of? @autocmd "autocmd" "api.nvim_create_autocmd" "vim.api.nvim_create_autocmd")
-)
---]]
 autocmd({"BufRead", "BufNewFile"}, {
   group = M.scala,
   pattern = "*.sbt",
   command = "set filetype=scala"
 })
-
-function foo() end
-
-foo()
 
 return M
