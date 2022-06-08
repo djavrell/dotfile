bd88a4a3ffac0fdd972688d2b2d3906d0db6a319
Prouteau Kévin <kevin.prouteau1@gmail.com>
2022-06-08 11:23:32 +0200

    [nvim] translate autocmd from viml to lua api

--- nvim/lua/djavrell/auto_cmd.lua
+++ nvim/lua/djavrell/auto_cmd.lua
@@ -6,7 +6,12 @@ local opt = { clear = true }
 local M = {
   nvimTree = ag("nvimTree", opt),
   yank = ag("highliht_yank", opt),
-  scala = ag("Scala", opt)
+  scala = ag("Scala", opt),
+  gpg = ag("GPG", opt),
+  troll = ag("TrollStopper", opt),
+  global = ag("Global", opt),
+  help = ag("HelpFile", opt),
+  envRC = ag("EnvRC", opt)
 }
 
 vim.api.nvim_create_autocmd("BufEnter", {
@@ -30,6 +35,49 @@ autocmd({"BufRead", "BufNewFile"}, {
   group = M.scala,
   pattern = "*.sbt",
   command = "set filetype=scala"
+})
+
+autocmd("User", {
+  group = M.gpg,
+  pattern = "GnuPG",
+  command = "setl textwidth=72"
+})
+
+autocmd("ColorScheme", {
+  group = M.troll,
+  pattern = "*",
+  command = "highlight TrollStopper ctermbg=red guibg=#FF0000"
+})
+
+autocmd("BufRead", {
+  group = M.global,
+  pattern = "*.conf",
+  command = "set ft=conf"
+})
+
+vim.cmd [[
+  augroup Global
+    autocmd!
+    autocmd BufRead *.conf set ft=conf
+    autocmd BufWritePre * :%s/\s\+$//e
+  augroup END
+]]
+-- autocmd("BufWritePre", {
+--   group = M.global,
+--   pattern = "*",
+--   command = ":%s/\s\+$//e"
+-- })
+
+autocmd("BufEnter", {
+  group = M.help,
+  pattern = "*",
+  command = "if &buftype ==# 'help' | nnoremap gd <C-]> | endif"
+})
+
+autocmd("BufRead", {
+  group = M.envRC,
+  pattern = { ".envrc", ".env.*", ".env" },
+  command = "set ft=sh"
 })
 
 return M
