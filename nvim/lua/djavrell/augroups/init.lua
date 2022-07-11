db134c5e4712fc7d8d8d909ed2897e224eca27e5
Prouteau Kévin <kevin.prouteau1@gmail.com>
2022-07-11 17:45:25 +0200

    [nvim] close NvimTree win if it is the last one open in current tabpage

--- nvim/lua/djavrell/augroups/init.lua
+++ nvim/lua/djavrell/augroups/init.lua
@@ -5,7 +5,7 @@ autocmd("BufEnter", {
   group = augroups["nvimTree"],
   nested = true,
   callback = function()
-    if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match('NvimTree_') ~= nil then
+    if #vim.api.nvim_tabpage_list_wins(0) == 1 and vim.api.nvim_buf_get_name(0):match('NvimTree_') ~= nil then
       vim.cmd "quit"
     end
   end,
