abac85cdc6ac8937a54abf51c160f1b3971ce11f
Prouteau Kévin <kevin.prouteau1@gmail.com>
2022-08-16 17:19:49 +0200

    [qmk][wip] add sometap-hold mapping

--- keymap/keymap.c
+++ keymap/keymap.c
@@ -11,8 +11,13 @@ enum layers {
     MDIA,  // media keys
 };
 
+// Layer switching
 #define LT1_F LT(SYMB, FR_F)
 #define LT1_J LT(SYMB, FR_J)
+
+// Tap-Hold keys
+#define MT1_SHS MT(MOD_LSFT, KC_SPC)
+#define MT1_CBS MT(MOD_LCTL, KC_BSPC)
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -24,14 +29,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_NO,    KC_NO,        KC_NO,  KC_LEFT,  KC_RGHT,                                                KC_UP,    KC_DOWN,  KC_NO,    KC_NO,          KC_NO,
                                                     KC_RALT,  KC_DEL,             KC_LALT, KC_NO,
                                                               KC_HOME,            KC_PGUP,
-                                           KC_SPC, KC_BSPC,   KC_ESC,             KC_PGDN, KC_TAB, KC_ENT
+                                            MT1_SHS, MT1_CBS, KC_ESC,             KC_PGDN, KC_TAB, KC_ENT
 ),
 [SYMB] = LAYOUT_ergodox_pretty(
   _______, _______, _______, _______, _______,  _______, _______,     _______, _______, _______,  _______,  _______,  _______,  _______,
   _______, FR_EXLM, FR_AT,   TD_CBR,  TD_ABK,   FR_PIPE, _______,     _______, KC_NO,   FR_7,     FR_8,     FR_9,     FR_ASTR,  _______,
   _______, FR_HASH, FR_DLR,  TD_PRN,  _______,  FR_GRV,                        KC_NO,   FR_4,     FR_5,     FR_6,     FR_PLUS,  _______,
   _______, FR_PERC, FR_CIRC, TD_BRC,  _______,  FR_TILD, _______,     _______, KC_NO,   FR_1,     FR_2,     FR_3,     KC_BSLS,  _______,
-  _______, _______, _______, _______, _______,                                          KC_NO,    KC_DOT,   FR_0,     KC_EQL,   _______,
+  _______, _______, _______, _______, _______,                                          _______,  _______,  FR_0,     KC_EQL,   _______,
                                                RGB_MOD, _______,     RGB_TOG, RGB_M_P,
                                                         _______,     _______,
                                        KC_SPC, KC_BSPC, _______,     _______, RGB_HUD, RGB_HUI
