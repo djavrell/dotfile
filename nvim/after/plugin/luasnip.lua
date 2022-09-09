96ab40e1fcb75a1fab04eb4b7aa231b9790f5544
Prouteau Kévin <kevin.prouteau1@gmail.com>
2022-09-09 17:02:00 +0200

    [nvim] luasnip trial

--- nvim/after/plugin/luasnip.lua
+++ nvim/after/plugin/luasnip.lua
@@ -35,16 +35,57 @@ vim.keymap.set("i", "<c-s>", function()
   end
 end)
 
+local l = require("luasnip.extras").lambda
+local pf = require("luasnip.extras.postfix").postfix
+
 local s = ls.snippet
 local t = ls.text_node
 local c = ls.choice_node
 local i = ls.insert_node
+local d = ls.dynamic_node
 local sn = ls.snippet_node
 
+
+--[[ local rec_ls
+rec_ls = function()
+	return sn(
+		nil,
+		c(1, {
+			-- Order is important, sn(...) first would cause infinite loop of expansion.
+			t(""),
+			sn(nil, { t({ "", "\t\\item " }), i(1), d(2, rec_ls, {}) }),
+		})
+	)
+end
+
 ls.add_snippets("typescript", {
-  s("trn", {
-    i(1), t(" ? "), i(2), t(" : "), i(0)
-  }),
+	-- rec_ls is self-referencing. That makes this snippet 'infinite' eg. have as many
+	-- \item as necessary by utilizing a choiceNode.
+	s("ls", {
+		t({ "\\begin{itemize}", "\t\\item " }),
+		i(1),
+		d(2, rec_ls, {}),
+		t({ "", "\\end{itemize}" }),
+	}),
+}) ]]
+
+local desc_rec
+desc_rec = function()
+  return sn(
+    nil,
+    c(1, {
+      t(""),
+      sn(nil, { t(", "), i(1) }),
+      sn(nil, { t(", ..."), i(1, "rest") }),
+      sn(nil, {
+        t(", "),
+        d(1, desc_rec, {})
+      }),
+    })
+  )
+end
+
+ls.add_snippets("typescript", {
   s("cl", {
     t("console.log("),
     c(1, {
@@ -56,5 +97,13 @@ ls.add_snippets("typescript", {
       })
     }),
     t(")")
+  }),
+  pf(".desc", {
+    l("[" .. l.POSTFIX_MATCH),
+    d(1, desc_rec, {}),
+    t("]"),
+  }),
+  s("desc", {
+    t("const [")
   })
 })
