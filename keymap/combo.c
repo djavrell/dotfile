6647f4bf764194d4d35b264e76a5ee9a3bd0eb36
Prouteau Kévin <kevin.prouteau1@gmail.com>
2022-10-10 14:08:02 +0200

    [qmk]: wip combo

--- keymap/combo.c
+++ keymap/combo.c
@@ -1,9 +1,14 @@
 #include QMK_KEYBOARD_H
 
+#include "keymap_french.h"
 #include "combo.h"
 
-unit16_t COMBO_LEN = COMBO_LENGTH;
+const uint16_t COMBO_LEN = COMBO_LENGTH;
 
-unit16_t PROGREM combos_map[][3] = {};
+const uint16_t PROGMEM combos_map[][3] = {
+  [CB_E_ACU] = { R_ACU, FR_E, COMBO_END }
+};
 
-combo_t key_combos[] = {};
+combo_t key_combos[] = {
+  COMB(CB_E_ACU, FR_EACU)
+};
--- keymap/combo.h
+++ keymap/combo.h
@@ -1,8 +1,12 @@
 #include QMK_KEYBOARD_H
 #include "keymap_french.h"
 
+#define COMB(cb, replace)  COMBO(combos_map[cb], replace)
+
+#define R_ACU  FR_J    // '
 
 enum combo_keys {
+  CB_E_ACU,
   COMBO_LENGTH,
 };
 
