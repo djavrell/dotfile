ef8a67440698ba8399b6ca67c2176bf863045632
Prouteau Kévin <kevin.prouteau1@gmail.com>
2022-11-07 11:07:04 +0100

    [nvim]: update TS import snip

--- nvim/lua/djavrell/snippets/ft/typescript/imports.lua
+++ nvim/lua/djavrell/snippets/ft/typescript/imports.lua
@@ -1,14 +1,29 @@
 local ls = require("luasnip")
+local l = require("luasnip.extras").lambda
 
 local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
+local c = ls.choice_node
+local sn = ls.snippet_node
 
 ls.add_snippets("typescript", {
-  s("import", {
-    t("import {"),
-    i(2),
-    t("} from '"),
+  s("imp", {
+    t("import "),
+    c(2, {
+      i(1, "*"),
+      sn(nil, {
+        t("{ "),
+        i(1),
+        t(" }")
+      }),
+      sn(nil, {
+        t("type { "),
+        i(1),
+        t(" }")
+      })
+    }),
+    t(" from '"),
     i(1),
     t("';")
   })
