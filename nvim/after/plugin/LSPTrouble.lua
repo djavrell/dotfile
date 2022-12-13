ac584769c95102eb7cbd9356f9c83c99c58dc3c6
Prouteau Kévin <kevin.prouteau1@gmail.com>
2022-12-13 16:57:56 +0100

    [nvim] Trouble global diagnostic

--- nvim/after/plugin/LSPTrouble.lua
+++ nvim/after/plugin/LSPTrouble.lua
@@ -1,4 +1,5 @@
 require("trouble").setup {
+  mode = "workspace_diagnostics",
   height = 20,
   auto_preview = false,
   auto_jump = {"lsp_definitions", "workspace_diagnostics", "document_diagnostics"}
