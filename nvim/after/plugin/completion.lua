305ae5a0f4e0b775e6261bdafddea422475d2431
Prouteau Kévin <kevin.prouteau1@gmail.com>
2023-02-20 14:45:45 +0100

    [nvim]: upate lua lsp

--- nvim/after/plugin/completion.lua
+++ nvim/after/plugin/completion.lua
@@ -169,7 +169,6 @@ cmp.setup.filetype('prompt', {
 local servers = {
   'tsserver',
   'bashls',
-  'sumneko_lua',
   'clangd',
   'jsonls'
 }
@@ -183,6 +182,29 @@ for _, lsp in ipairs(servers) do
     },
   }
 end
+
+nvim_lsp.lua_ls.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
 
 _ = vim.cmd [[
   augroup DadbodSql
