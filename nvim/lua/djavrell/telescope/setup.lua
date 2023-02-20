7fae92b98916eb3228c025a16ff54c6820e0fb7e
Prouteau Kévin <kevin.prouteau1@gmail.com>
2023-02-20 14:48:49 +0100

    [nvim]: fix telescope conf

--- nvim/lua/djavrell/telescope/setup.lua
+++ nvim/lua/djavrell/telescope/setup.lua
@@ -1,7 +1,6 @@
 local Telescope = require('telescope')
 local actions = require('telescope.actions')
 local actions_layout = require('telescope.actions.layout')
-local lga_actions = require("telescope-live-grep-args.actions")
 
 Telescope.setup {
   defaults = {
