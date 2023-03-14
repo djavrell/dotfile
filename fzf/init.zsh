742626a9e58adc501856c55b60c86bd7110fe787
Prouteau Kévin <kevin.prouteau1@gmail.com>
2023-03-14 13:40:25 +0100

    [zsh] update fzf history binding/logic

--- fzf/init.zsh
+++ fzf/init.zsh
@@ -1,7 +1,16 @@
 # FZF fuzzy finder
-export FZF_DEFAULT_COMMAND="fd --type f --hidden --follow --ignore-file ~/.bashrc.d/fdignore"
+export FZF_DEFAULT_COMMAND="fd --type f --hidden --follow --ignore-file $DOTFILE/fdignore"
 export FZF_COMPLETION_TRIGGER='%'
 
-load "$DOTFILE/fzf/fzf.zsh"
+# export FZF_CTRL_R_OPTS='--reverse'
 
+load "$DOTFILE/fzf/fzf.zsh"
 load "$SUB_MODULES/fzf-marks/init.zsh"
+
+fzf-history-widget-accept() {
+  fzf-history-widget
+  zle accept-line
+}
+
+zle     -N   fzf-history-widget-accept
+bindkey '^R' fzf-history-widget-accept
