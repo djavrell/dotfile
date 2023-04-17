42fb6fd43d044741da67452a7d3ece8a58965d0b
Prouteau Kévin <kevin.prouteau1@gmail.com>
2023-04-17 13:44:25 +0200

    [QMK] move numpad on layer 4 + add alt key

--- keymap/config.h
+++ keymap/config.h
@@ -8,9 +8,12 @@
 // Layer switching
 #define LT1_F LT(1, FR_F)
 #define LT1_J LT(1, FR_J)
+#define LT3_D LT(3, FR_D)
 
 // Tap-Hold keys
 #define MT1_SHS MT(MOD_LSFT, KC_SPC)
 #define MT1_CBS MT(MOD_LCTL, KC_BSPC)
 #define MT1_ABS MT(MOD_LALT, KC_BSPC)
 #define MT2_CTB MT(MOD_LCTL, KC_TAB)
+#define MT2_AEN MT(MOD_LALT, KC_ENT)
+
