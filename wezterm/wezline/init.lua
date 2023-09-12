5f8f357a79facab40ea39c285601579c4642b441
Prouteau Kévin <kevin.prouteau1@gmail.com>
2023-09-12 14:54:42 +0200

    [wezterm] use meta file for statusline conf

--- wezterm/wezline/init.lua
+++ wezterm/wezline/init.lua
@@ -1,19 +1,11 @@
-local wezterm = require("wezterm")
+local wezterm = require('wezterm')
 local G = wezterm.GLOBAL
 local NF = wezterm.nerdfonts
 local T = require('utils.merge')
 
 local M = {}
 
-local namespace = "wezline"
-
----@alias IconeTable table<string, string>
-
----@class StatuslineOpt
----@field key_table_icone IconeTable|nil
-
----@class StatuslineConfig
----@field key_table_icone IconeTable
+local namespace = 'wezline'
 
 ---@type StatuslineConfig
 local defaultConfig = {
---
+++ wezterm/wezline/types.d.lua
@@ -0,0 +1,10 @@
+---@meta wezline
+-- Configuration object for status line module
+
+---@alias IconeTable table<string, string>
+
+---@class StatuslineOpt
+---@field key_table_icone IconeTable|nil
+
+---@class StatuslineConfig
+---@field key_table_icone IconeTable
