1c95c8ca1cae1fa6bd5f03b57ce23485c228d27d
Prouteau Kévin <kevin.prouteau1@gmail.com>
2024-02-12 16:00:18 +0100

    [nvim] add table helper to vim-dadbob

--- nvim/lua/plugins/dadbob.lua
+++ nvim/lua/plugins/dadbob.lua
@@ -8,6 +8,17 @@ return {
       vim.g.db_ui_auto_execute_table_helpers = 1
     end
   },
-  'kristijanhusak/vim-dadbod-ui',
+  {
+    'kristijanhusak/vim-dadbod-ui',
+    config = function()
+      vim.cmd[[
+        let g:db_ui_table_helpers = {
+        \   'postgresql': {
+        \     'Constraints': "SELECT conrelid::regclass AS table_from, conname, pg_get_constraintdef(oid) FROM  pg_constraint WHERE contype IN ('f', 'p') AND conrelid = '{table}'::regclass AND connamespace = 'public'::regnamespace ORDER BY conrelid::regclass::text, contype DESC;"
+        \   }
+        \ }
+      ]]
+    end
+  },
   'kristijanhusak/vim-dadbod-completion',
 }
