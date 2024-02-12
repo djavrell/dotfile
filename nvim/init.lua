9bbf3e3b5586f4091038b2a4f3a0cc9361169701
Prouteau Kévin <kevin.prouteau1@gmail.com>
2024-02-12 16:01:34 +0100

    [nvim] move leader definition

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,13 @@
+vim.cmd([[
+  try
+    source $XDG_CONFIG_HOME/local_nvim.vim
+  catch
+    let mapleader = '='
+  endtry
+
+  filetype plugin indent on
+]])
+
 require('djavrell.globals')
 require('djavrell.lazy_init')
 require('djavrell.augroups')
-
-require('djavrell.telescope.setup')
-require('djavrell.telescope.mapping')
--- nvim/lua/djavrell/options.lua
+++ nvim/lua/djavrell/options.lua
@@ -1,13 +1,2 @@
 vim.opt_global.completopt = { "menu", "menuone", "noselect" }
 vim.opt_global.shortmess:remove("F"):append("c")
-
-vim.cmd([[
-  try
-    source $XDG_CONFIG_HOME/local_nvim.vim
-  catch
-    let mapleader = '='
-  endtry
-
-  filetype plugin indent on
-]])
-
