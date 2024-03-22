ccca12828b8e1365e37478bd7c24995135873b08
Prouteau Kévin <kevin.prouteau1@gmail.com>
2024-03-22 17:58:49 +0100

    [nvim] add fugit2

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -101,4 +101,21 @@ return {
       })
     end
   },
+  {
+    'SuperBo/fugit2.nvim',
+    opts = {},
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      'nvim-tree/nvim-web-devicons',
+      'nvim-lua/plenary.nvim',
+      {
+        'chrisgrieser/nvim-tinygit',
+        dependencies = { 'stevearc/dressing.nvim' }
+      }
+    },
+    cmd = { 'Fugit2', 'Fugit2Graph' },
+    keys = {
+      { '<leader>F', mode = 'n', '<cmd>Fugit2<cr>' }
+    }
+  },
 }
