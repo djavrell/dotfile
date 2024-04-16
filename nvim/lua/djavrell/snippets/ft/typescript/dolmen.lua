afae71ce4dd3b6a4dd9f3df183887a82dc057a69
Prouteau Kévin <kevin.prouteau1@gmail.com>
2024-04-16 15:24:50 +0200

    [nvim] add dolmen specific snip

--- nvim/lua/djavrell/snippets/ft/typescript/dolmen.lua
+++ nvim/lua/djavrell/snippets/ft/typescript/dolmen.lua
@@ -3,13 +3,13 @@ local ls = require("luasnip")
 local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
+local r = ls.restore_node
 local c = ls.choice_node
+local sn = ls.snippet_node
 local fmt = require("luasnip.extras.fmt").fmt
 
 ls.add_snippets("typescript", {
-  s(
-    "dol-error",
-    fmt([[
+  s("dol-error", fmt([[
       import { z } from 'zod';
 
       const schema = z.object({
@@ -43,5 +43,51 @@ ls.add_snippets("typescript", {
     err = i(2, "undefined"),
   }, {
     delimiters = "[]",
-  }))
+  })),
+  s(
+    "schema",
+    fmt(
+      [[
+        import { z } from 'zod';
+
+        const [name] = z.object([content]);
+
+        export type [type] = z.infer<typeof [name]>;
+
+        export const [type] = { [name] };
+      ]],
+      {
+        type = i(1),
+        name = i(2, "schema"),
+        -- content = i(3),
+        content = c(3, {
+          sn(nil, {
+            t("object({"),
+            r(1, "obj_content"),
+            t("})")
+          }),
+          sn(nil, {
+            t("array(["),
+            r(1, "arr_content"),
+            t("])")
+          }),
+          sn(nil, {
+            t("array("),
+            r(1, "arr_content_empty"),
+            t(")")
+          }),
+        }),
+
+      },
+      {
+        delimiters = "[]",
+        repeat_duplicates = true,
+      }
+    ),
+    {
+      ["obj_content"] = i(1),
+      ["arr_content"] = i(1),
+      ["arr_content_empty"] = i(1)
+    }
+  )
 })
