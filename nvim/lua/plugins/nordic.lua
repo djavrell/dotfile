7b59768d1263ed8c70be26ede0a5068c791ab18a
Prouteau Kévin <kevin.prouteau1@gmail.com>
2024-04-16 15:24:50 +0200

    [nvim] update nordic opt

--- nvim/lua/plugins/nordic.lua
+++ nvim/lua/plugins/nordic.lua
@@ -1,12 +1,17 @@
 return {
   'AlexvZyl/nordic.nvim',
   opts = {
+    bold_keywords = true,
+    reduced_blue = true,
     cursorline = {
       theme = 'light',
       blend = 1,
     },
     telescope = {
       style = 'flat'
+    },
+    ts_context = {
+      dark_background = true,
     },
     override = {
       ['@keyword.coroutine'] = { link = 'Keyword' },
