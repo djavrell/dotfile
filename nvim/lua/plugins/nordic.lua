e23a1e942efca7b85690d7ae932fa2dddccf20d4
Prouteau Kévin <kevin.prouteau1@gmail.com>
2024-04-16 16:13:06 +0200

    [nvim] override FloatBoarder highlight

--- nvim/lua/plugins/nordic.lua
+++ nvim/lua/plugins/nordic.lua
@@ -1,21 +1,28 @@
 return {
   'AlexvZyl/nordic.nvim',
-  opts = {
-    bold_keywords = true,
-    reduced_blue = true,
-    cursorline = {
-      theme = 'light',
-      blend = 1,
-    },
-    telescope = {
-      style = 'flat'
-    },
-    ts_context = {
-      dark_background = true,
-    },
-    override = {
-      ['@keyword.coroutine'] = { link = 'Keyword' },
-      ['@lsp.type.interface'] = { link = 'Type' }
+  opts = function()
+    local C = require 'nordic.colors'
+    return {
+      bold_keywords = true,
+      reduced_blue = true,
+      cursorline = {
+        theme = 'light',
+        blend = 1,
+      },
+      telescope = {
+        style = 'flat'
+      },
+      ts_context = {
+        dark_background = true,
+      },
+      override = {
+        ['@keyword.coroutine'] = { link = 'Keyword' },
+        ['@lsp.type.interface'] = { link = 'Type' },
+        FloatBorder = {
+          fg = C.fg_sidebar,
+          bg = C.bg
+        }
+      }
     }
-  }
+  end
 }
