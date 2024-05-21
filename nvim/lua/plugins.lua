ade9eff64d76fd552f94e91abe1ab8622b395265
Prouteau Kévin <kevin.prouteau1@gmail.com>
2024-05-21 11:00:19 +0200

    [nvim] add markdown renderer plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -144,6 +144,20 @@ return {
     end,
   },
 
+  {
+    'MeanderingProgrammer/markdown.nvim',
+    name = 'render-markdown', -- Only needed if you have another plugin named markdown.nvim
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    config = function()
+        require('render-markdown').setup({
+          highlights = {
+            heading = {
+              backgrounds = {}
+            }
+          }
+        })
+    end,
+  },
 
   {
     'anuvyklack/hydra.nvim',
