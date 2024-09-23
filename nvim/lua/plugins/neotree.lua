b2e99cf408b86e840a945b7b0f1e9b35d37bab5a
Prouteau Kévin <kevin.prouteau1@gmail.com>
2024-09-23 09:26:46 +0200

    [nvim][wip] add config for neotree

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -1,4 +1,28 @@
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  cmd = { 'Neotree' }
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+      dependencies = {
+        'MunifTanjim/nui.nvim',
+        'nvim-tree/nvim-web-devicons',
+        'nvim-lua/plenary.nvim',
+      },
+    cmd = { 'Neotree' },
+    config = function()
+      require('neo-tree').setup({
+        close_if_last_window = true,
+        source_selector = {
+          winbar = true,
+          statusline = false
+        }
+      })
+    end,
+  },
+  {
+    's1n7ax/nvim-window-picker',
+    name = 'window-picker',
+    event = 'VeryLazy',
+    config = function()
+        require'window-picker'.setup()
+    end,
+  }
 }
