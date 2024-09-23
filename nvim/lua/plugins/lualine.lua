843f9bf2af1c2fc48cbdb0c0523df5c19abba886
Prouteau Kévin <kevin.prouteau1@gmail.com>
2024-09-23 15:20:43 +0200

    [nvim] add lualine lsp status module

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,10 +1,19 @@
 return {
   {
     'nvim-lualine/lualine.nvim',
+    dependencies = {
+      'pnx/lualine-lsp-status'
+    },
     config = function()
       require('lualine').setup({
         options = {
           theme = 'nordic'
+        },
+        sections = {
+          lualine_b = { 'branch', 'diagnostics' },
+          lualine_x = {
+            'lsp-status', 'filetype'
+          }
         }
       })
     end
