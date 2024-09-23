f4f7972a10c383e7969f75021295775100f110c3
Prouteau Kévin <kevin.prouteau1@gmail.com>
2024-09-23 19:04:32 +0200

    [nvim] use neotree instead of nvim-tree

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -7,12 +7,33 @@ return {
         'nvim-lua/plenary.nvim',
       },
     cmd = { 'Neotree' },
+    keys = {
+      { '<leader>w', '<cmd>Neotree toggle<CR>', 'n', { silent = true, noremap = true } },
+      { '<leader>x', '<cmd>Neotree focus<CR>', 'n', { silent = true, noremap = true } },
+    },
     config = function()
       require('neo-tree').setup({
         close_if_last_window = true,
         source_selector = {
           winbar = true,
           statusline = false
+        },
+        filesystem = {
+          follow_current_file = {
+            enabled = true,
+            leave_dirs_open = true
+          },
+          window = {
+            width = 30
+          }
+        },
+        buffers = {
+          follow_current_file = {
+            enabled = true,
+            leave_dirs_open = false,
+          },
+          group_empty_dirs = true,
+          show_unloaded = true,
         }
       })
     end,
