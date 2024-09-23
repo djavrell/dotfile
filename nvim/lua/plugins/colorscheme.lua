519a5d35deb846888fa7b5521a3d82b1fbe9d8cf
Prouteau Kévin <kevin.prouteau1@gmail.com>
2024-09-23 15:44:39 +0200

    [nvim] rename nordic file to colorscheme

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    'AlexvZyl/nordic.nvim',
+    opts = function()
+      local C = require 'nordic.colors'
+      return {
+        bold_keywords = true,
+        reduced_blue = true,
+        cursorline = {
+          theme = 'light',
+          blend = 1,
+        },
+        telescope = {
+          style = 'flat'
+        },
+        ts_context = {
+          dark_background = true,
+        },
+        override = {
+          ['@keyword.coroutine'] = { link = 'Keyword' },
+          ['@lsp.type.interface'] = { link = 'Type' },
+          FloatBorder = {
+            fg = C.fg_sidebar,
+            bg = C.bg
+          },
+          LazyNormal = {
+            bg = C.bg,
+          },
+        }
+      }
+    end
+  },
+}
