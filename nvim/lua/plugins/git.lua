37f3f825286430d21de7f7b8fb4e22865537fa0e
Prouteau Kévin <kevin.prouteau1@gmail.com>
2024-09-23 09:26:46 +0200

    [nvim] add octo plugin

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -145,4 +145,28 @@ return {
       },
     },
   },
+  {
+    'pwntester/octo.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope.nvim',
+      'nvim-tree/nvim-web-devicons',
+    },
+    config = function ()
+      require"octo".setup({
+        suppress_missing_scope = {
+          projects_v2 = true,
+        },
+        mappings = {
+          review_diff = {
+            select_next_entry = { lhs = "<space>n", desc = "move to next changed file" },
+            select_prev_entry = { lhs = "<space>p", desc = "move to previous changed file" },
+            select_first_entry = { lhs = "<space>N", desc = "move to first changed file" },
+            select_last_entry = { lhs = "<space>P", desc = "move to last changed file" },
+          }
+        }
+      })
+      vim.treesitter.language.register('markdown', 'octo')
+    end
+  }
 }
