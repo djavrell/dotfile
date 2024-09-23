d20d5fc913b10fb1a9728cd247db0e2feb5b0cf6
Prouteau Kévin <kevin.prouteau1@gmail.com>
2024-09-23 09:26:46 +0200

    [nvim] update completion mapping

--- nvim/after/plugin/completion.lua
+++ nvim/after/plugin/completion.lua
@@ -23,8 +23,8 @@ cmp.setup({
     end,
   },
   mapping = {
-    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c', 'n' }),
+    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c', 'n' }),
     ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
     ['<C-e>'] = cmp.mapping({
       i = cmp.mapping.abort(),
