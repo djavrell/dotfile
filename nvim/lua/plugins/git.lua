b642a5e580b1ecaa31390c150482e35e5be6bd5d
Prouteau Kévin <kevin.prouteau1@gmail.com>
2025-05-19 15:14:41 +0200

    [nvim] move diffview config into git file

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,4 +1,66 @@
 return {
+  {
+    'sindrets/diffview.nvim',
+    config = function()
+      local actions = require('diffview.config').actions
+
+      require('diffview').setup({
+        diff_binaries = false, -- Show diffs for binaries
+        use_icons = true, -- Requires nvim-web-devicons
+        file_panel = {
+          win_config = {
+            width = 35,
+          },
+        },
+        view = {
+          merge_tool = {
+            layout = 'diff4_mixed',
+            disable_diagnostics = true,
+          },
+        },
+        hooks = {
+          view_opened = function()
+            -- h:activate()
+          end,
+          view_closed = function()
+            -- h:exit()
+          end,
+        },
+        key_bindings = {
+          -- The `view` bindings are active in the diff buffers, only when the current
+          -- tabpage is a Diffview.
+          view = {
+            ['<tab>'] = actions.select_next_entry, -- Open the diff for the next file
+            ['<s-tab>'] = actions.select_prev_entry, -- Open the diff for the previous file
+            ['<leader>e'] = actions.focus_files, -- Bring focus to the files panel
+            ['<leader>b'] = actions.toggle_files, -- Toggle the files panel.
+          },
+          file_panel = {
+            ['j'] = actions.next_entry, -- Bring the cursor to the next file entry
+            ['<down>'] = actions.next_entry,
+            ['k'] = actions.prev_entry, -- Bring the cursor to the previous file entry.
+            ['<up>'] = actions.prev_entry,
+            ['<cr>'] = actions.select_entry, -- Open the diff for the selected entry.
+            ['o'] = actions.select_entry,
+            ['R'] = actions.refresh_files, -- Update stats and entries in the file list.
+            ['<tab>'] = actions.select_next_entry,
+            ['<s-tab>'] = actions.select_prev_entry,
+            ['<leader>e'] = actions.focus_files,
+            ['<leader>b'] = actions.toggle_files,
+          },
+          diff4 = {
+            { { 'n', 'x' }, '<leader>co', actions.conflict_choose('ours') },
+            { { 'n', 'x' }, '<leader>ct', actions.conflict_choose('theirs') },
+            { { 'n', 'x' }, '<leader>cb', actions.conflict_choose('base') },
+            { { 'n', 'x' }, '<leader>ca', actions.conflict_choose('all') },
+            { { 'n', 'x' }, 'dx', actions.conflict_choose('none') },
+            { { 'n', 'x' }, '<leader>n', actions.next_conflict },
+            { { 'n', 'x' }, '<leader>p', actions.prev_conflict },
+          },
+        },
+      })
+    end,
+  },
   'rhysd/git-messenger.vim',
   {
     'rhysd/committia.vim',
@@ -58,7 +120,6 @@ return {
   },
   {
     'isakbm/gitgraph.nvim',
-    ---@type I.GGConfig
     opts = {
       symbols = {
         merge_commit = 'M',
