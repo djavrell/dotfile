5d2f02afac355109a7780eb23e6b9c26edb45f97
Prouteau Kévin <kevin.prouteau1@gmail.com>
2025-07-30 18:00:41 +0200

    [nvim] update markview conf

--- nvim/lua/plugins/ft-randerer.lua
+++ nvim/lua/plugins/ft-randerer.lua
@@ -18,6 +18,38 @@ return {
         },
         experimental = { check_rtp_message = false },
       })
+
+      require('markview.extras.checkboxes').setup({
+        --- Default checkbox state(used when adding checkboxes).
+        ---@type string
+        default = 'X',
+
+        --- Changes how checkboxes are removed.
+        ---@type
+        ---| "disable" Disables the checkbox.
+        ---| "checkbox" Removes the checkbox.
+        ---| "list_item" Removes the list item markers too.
+        remove_style = 'disable',
+
+        --- Various checkbox states.
+        ---
+        --- States are in sets to quickly change between them
+        --- when there are a lot of states.
+        ---@type string[][]
+        states = {
+          { ' ', '/', 'X' },
+          { '<', '>' },
+          { '?', '!', '*' },
+          { '"' },
+          { 'l', 'b', 'i' },
+          { 'S', 'I' },
+          { 'p', 'c' },
+          { 'f', 'k', 'w' },
+          { 'u', 'd' },
+        },
+      })
+      require('markview.extras.headings').setup()
+      require('markview.extras.editor').setup()
     end,
   },
   {
