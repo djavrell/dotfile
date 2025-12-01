2dabd79db0b91e1208b5a877918ccafda2e41346
Prouteau Kévin <kevin.prouteau1@gmail.com>
2025-12-01 09:46:01 +0100

    [zsh] fix export in ssh module

--- ssh/init.zsh
+++ ssh/init.zsh
@@ -1,7 +1,5 @@
-local SSH_HOME="$$HOME/.ssh/id_rsa"
-
 if [ -d "$WSL_DISTRO_NAME" ]; then
-  SSH_HOME="$W_HOME/.ssh/id_rsa"
+  export SSH_KEY_PATH="$W_HOME/.ssh/id_rsa"
 else
-
-export SSH_KEY_PATH="$SSH_HOME"
+  export SSH_KEY_PATH="$HOME/.ssh/id_rsa"
+fi
