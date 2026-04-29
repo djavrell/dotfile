474c410ba8baae3adc33b993140cc3161c957f50
Prouteau Kévin <kevin.prouteau1@gmail.com>
2026-04-29 09:56:09 +0200

    [nvim] misc cleanup comment

--- nvim/lua/plugins/lazydev.lua
+++ nvim/lua/plugins/lazydev.lua
@@ -39,15 +39,6 @@ return {
         },
       })
     end,
-    --[[ opts = {
-      library = {
-        { path = 'luvit-meta/library', words = { 'vim%.uv' } },
-        'lazy.nvim',
-        { path = 'luvit-meta/library', words = { 'vim%.uv' } },
-        { path = 'LazyVim', words = { 'LazyVim' } },
-        { path = 'wezterm-types', mods = { 'wezterm' } },
-      },
-    }, ]]
   },
   { 'Bilal2453/luvit-meta', lazy = true },
 }
