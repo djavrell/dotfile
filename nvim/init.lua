befd2600356da1268b8394e8bb3e462ee15217c2
Prouteau Kévin <kevin.prouteau1@gmail.com>
2026-09-11 11:07:42 +0200

    [nvim] update ui2 conf

--- nvim/init.lua
+++ nvim/init.lua
@@ -23,18 +23,18 @@ require('vim._core.ui2').enable({
     ---@type string|table<string, 'cmd'|'msg'|'pager'> Default message target
     ---or table mapping |ui-messages| kinds and triggers to a target.
     targets = 'cmd',
-    cmd = { -- Options related to messages in the cmdline window.
-      height = 0.5, -- Maximum height while expanded for messages beyond 'cmdheight'.
-    },
     dialog = { -- Options related to dialog window.
       height = 0.5, -- Maximum height.
     },
     msg = { -- Options related to msg window.
       height = 0.5, -- Maximum height.
-      timeout = 4000, -- Time a message is visible in the message window.
     },
     pager = { -- Options related to message window.
       height = 1, -- Maximum height.
     },
   },
 })
+
+vim.cmd([[
+    set messagesopt+=maxheight:50,timeout:4000
+]])
